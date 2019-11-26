d3922f8335015cf6c013734c4a14b43ce4efde49
PalakKatira <58144839+PalakKatira@users.noreply.github.com>
2019-11-26 19:43:24 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;;
+
+public class Fibonacci {
+	static int n1, n2, n3, n4, n5;
+
+	public static void printFibonacciSeries(int p) {
+		n1 = 0;
+		n2 = 1;
+		n3 = 1;
+		n4 = 2;
+		System.out.println("Fibonacci Series");
+		System.out.print(n1 + " " + n2 + " " + n3 + " " + n4);
+
+		for (int i = 4; i < p; i++) {
+			n5 = n1 + n2 + n3 + n4;
+			System.out.print(" " + n5);
+			n1 = n2;
+			n2 = n3;
+			n3 = n4;
+			n4 = n5;
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		int n;
+		System.out.println("Enter the value of n");
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		
+		printFibonacciSeries(n);//invoking static method
+	}
+
+}
