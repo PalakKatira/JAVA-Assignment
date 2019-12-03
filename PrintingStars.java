847ad4ab0b6a78a2a289fef10856008ebb2eb0b8
PalakKatira <58144839+PalakKatira@users.noreply.github.com>
2019-12-03 21:37:36 +0530

    Update PrintingStars.java

--- PrintingStars.java
+++ PrintingStars.java
@@ -1,12 +1,15 @@
+/*creating packages(lettes to be in lower case)
+This is done for logical grouping*/
+
 package com.test.assignment_25nov;
 
-import java.util.Scanner;
+import java.util.Scanner;//importing util class to invoke Scanner 
 
-public class PrintingStars {//class formation
-	public  void printPattern() {//instance method declaration
+public class PrintingStars {//class formation in pascal casing
+	public void printPattern() {//instance method declaration
 		int n;
 
-		System.out.println("Enter the value of n");//printing and hence asking the user to enter the value of n
+		System.out.println("Enter the value of n that is number of rows to printed");//printing and hence asking the user to enter the value of n
 
 		Scanner in = new Scanner(System.in);//Taking input from the user on how many rows to print
 		n = in.nextInt();
@@ -20,8 +23,8 @@ public class PrintingStars {//class formation
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//main method to invoke methods- always static
 		PrintingStars star = new PrintingStars();//Invoking the instance method by creating the object of the class
-		star.printPattern();//invoking by star operator
+		star.printPattern();//invoking by star object and dot operator
 	}
 }
