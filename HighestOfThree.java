fcf69cc8f3152b0a3c64e0568848f8b16009bca1
PalakKatira <58144839+PalakKatira@users.noreply.github.com>
2019-12-03 22:14:08 +0530

    Update HighestOfThree.java

--- HighestOfThree.java
+++ HighestOfThree.java
@@ -1,11 +1,11 @@
-package com.test.assignment_25nov;
+package com.test.assignment_25nov;//creation of package
 
-import java.util.Scanner;
+import java.util.Scanner;//1util class
 
-public class HighestOfThree {
-	int a, b, c;
+public class HighestOfThree {//class
+	int a, b, c;//member variables
 
-	public static int highestNumber() {
+	public static int highestNumber() {//static method to calculate the highest number
 		System.out.println("Enter the value of a,b,c");
 
 		Scanner num = new Scanner(System.in);
@@ -16,7 +16,7 @@ public class HighestOfThree {
 		if (a > b && a > c) {
 			System.out.println(a + " is the largest number");
 			return a;
-		} else if (b > c) {
+		} else if (b > c) {//b>a is already checked in the above condition
 			System.out.println(b + " is the largest number");
 			return c;
 		} else {
@@ -27,7 +27,7 @@ public class HighestOfThree {
 	}
 
 	public static void main(String[] args) {
-		highestNumber();
+		highestNumber();//calling directly the method
 	}
 
 }
