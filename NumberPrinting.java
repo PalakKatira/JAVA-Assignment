0b3fc92a7b495cff528f62341aae59beccc428e8
PalakKatira <58144839+PalakKatira@users.noreply.github.com>
2019-12-03 22:18:34 +0530

    Update NumberPrinting.java

--- NumberPrinting.java
+++ NumberPrinting.java
@@ -1,33 +1,33 @@
-package pattern;
+package pattern;//creating package
 
-import java.util.Scanner;
+import java.util.Scanner;//importing util class 
 
-public class NumberPrinting {
+public class NumberPrinting {//class
 	int r;
 	 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//main method
 	 
 		System.out.println("Enter the value of rows to be printed(odd values only)");
 		
-		Scanner sc=new Scanner(System.in);
+		Scanner sc=new Scanner(System.in);//taking input from the user
 		int r=sc.nextInt();
 		
-		int mid=(r+1)/2;
+		int mid=(r+1)/2;//calculating the mid value
 		
-		for(int i=1;i<=mid;i++) {
-			for(int j=1;j<=(mid-i);j++) {
+		for(int i=1;i<=mid;i++) {//printing the number of rows till middle row
+			for(int j=1;j<=(mid-i);j++) {//printing the spaces
 				System.out.println(" ");
 			}
-			for(int k=1;k<=(2*i-1);k++) {
+			for(int k=1;k<=(2*i-1);k++) {//printing the stars
 				System.out.print(i);
 			}
 			System.out.println();
 		}
-		for(int i=1;i<=(mid-1);i++){
-			for(int j=1;j<=i;j++) {
+		for(int i=1;i<=(mid-1);i++){//prints rows
+			for(int j=1;j<=i;j++) {//prints spaces
 				System.out.println(" ");
 			}
-			for(int k=1;k<=(2*(mid-i)-1);k++) {
+			for(int k=1;k<=(2*(mid-i)-1);k++) {//prints star
 				System.out.print(mid-i);
 			}
 		}
